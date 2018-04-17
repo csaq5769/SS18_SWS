22e53c9e5ddb4491c67348551c6da966af3117f2
csaq5769 <j.wanker@student.uibk.ac.at>
2018-04-17 13:29:35 +0200

    add ws

---
+++ src/ws/ProductArray.java
@@ -0,0 +1,69 @@
+
+package ws;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for productArray complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * )
+ * <pre>
+ * &lt;complexType name="productArray">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="item" type="{http://ServerSide/}product" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "productArray", propOrder = {
+    "item"
+})
+public class ProductArray {
+
+    @XmlElement(nillable = true)
+    protected List<Product> item;
+
+    /**
+     * Gets the value of the item property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the item property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getItem().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Product }
+     * 
+     * 
+     */
+    public List<Product> getItem() {
+        if (item == null) {
+            item = new ArrayList<Product>();
+        }
+        return this.item;
+    }
+
+}
